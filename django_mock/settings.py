9f7f2de42b7d33489299e0be01f958c8908be146
Saurav Ganguly <sauravganguly.in@gmail.com>
2022-10-04 08:22:14 +0530

    added hosts in allowed_hosts

--- django_mock/settings.py
+++ django_mock/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vrau8e*p$=@9eucp+92x!gsuv7ekkddxk4)=u03*fgc6ip9v$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
